caa248c28a6502afa9d9e6c642e0c161242c0971
Guilherme Lobato <guilobato2002@gmail.com>
2024-04-17 17:40:29 -0300

    cards da home page

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,58 +1,100 @@
 "use client"
-import React, { useState } from "react";
-import styles from "./homePage.module.css"
 
+import React, { useState, useEffect } from "react";
+import styles from "./homePage.module.css";
+import axios from "axios";
+
+interface Product {
+    title: string;
+    price: number;
+    description: string;
+    image: string;
+    category: string;
+}
 
 export default function HomePage() {
-    const [tableData, setTableData] = useState([
-        { title: "Guilherme", price: "Amor", description: "Muitos", image: "sds", category: "elotronic"},
-        { title: "Guilherme", price: "Amor", description: "Muitos", image: "sds", category: "elotronic"},
-      ]);
-    
-      const downloadCSV = () => {
+    const [productData, setProductData] = useState<Product[]>([]);
+    const [expandedProduct, setExpandedProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        importApi();
+    }, []);
+
+    const downloadCSV = () => {
         const csvContent =
-          "data:text/csv;charset=utf-8," +
-          tableData.map((row) => Object.values(row).join(",")).join("\n");
+            "data:text/csv;charset=utf-8," +
+            productData.map((product) => {
+                return (
+                    Object.values(product)
+                        .map((value) => (typeof value === "string" ? `"${value}"` : value))
+                        .join(",") + "\n"
+                );
+            }).join("");
         const encodedUri = encodeURI(csvContent);
         const link = document.createElement("a");
         link.setAttribute("href", encodedUri);
         link.setAttribute("download", "data.csv");
         document.body.appendChild(link);
         link.click();
-      };
+    };
 
-    return(
+    const importApi = async () => {
+        try {
+            const response = await axios.get("https://fakestoreapi.com/products");
+            setProductData((prevProducts) => [...prevProducts, ...response.data]);
+        } catch (error) {
+            console.log("Erro ao obter dados da API:", error);
+        }
+    };
+
+    const handleExpandProduct = (product: Product) => {
+        if (expandedProduct && expandedProduct.title === product.title) {
+            setExpandedProduct(null); // Fecha o card se já estiver expandido
+        } else {
+            setExpandedProduct(product); // Expandir o card com a descrição do produto
+        }
+    };
+
+    const handleDeleteProduct = (product: Product) => {
+        const updatedProducts = productData.filter((p) => p.title !== product.title);
+        setProductData(updatedProducts);
+    };
+
+    return (
         <div className={styles.background}>
             <div className={styles.menu}>
-                <button className={styles.button}>IMPORTAR ARQUIVO CSV</button>
                 <button className={styles.button} onClick={downloadCSV}>EXPORTAR ARQUIVO CSV</button>
-                <button className={styles.button}>CRIA NOVO CARD DE PRODUTO</button>
+                <label htmlFor="file-upload" className={styles.labelinput}>IMPORTAR ARQUIVO CSV</label>
+                <input id="file-upload" className={styles.inputcsv} type="file" accept="csv/CSV" />
+                <button className={styles.button} onClick={importApi}>IMPORTAR DADOS DA API</button>
+                <button className={styles.button}>ADICIONAR NOVO PRODUTO</button>
             </div>
 
             <div className={styles.container}>
-                <table className={styles.table}>
-                    <thead className={styles.thead}>
-                        <tr>
-                            <th className={styles.title}>Titulo</th>
-                            <th className={styles.price}>Preço</th>
-                            <th className={styles.description}>Descrição</th>
-                            <th className={styles.image}>Imagem</th>
-                            <th className={styles.category}>Categoria</th>
-                        </tr>
-                    </thead>
-                    <tbody className={styles.tbody}>
-                        {tableData.map((row, index) => (
-                        <tr key={index}>
-                            <td className={styles.title}>{row.title}</td>
-                            <td className={styles.price}>{row.price}</td>
-                            <td className={styles.description}>{row.description}</td>
-                            <td className={styles.image}>{row.image}</td>
-                            <td className={styles.category}>{row.category}</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <div className={styles.cardContainer}>
+                    {productData.map((product, index) => (
+                        <div className={styles.card} key={index}>
+                            <button className={styles.deleteButton} onClick={() => handleDeleteProduct(product)}>x</button>
+                            <img className={styles.cardImage} src={product.image} alt={product.title} />
+                            <div className={styles.cardContent}>
+                                <h3 className={styles.cardTitle}>{product.title}</h3>
+                                <p className={styles.cardPrice}>${product.price}</p>
+                                <button
+                                    className={styles.cardButton}
+                                    onClick={() => handleExpandProduct(product)}
+                                >
+                                    {expandedProduct && expandedProduct.title === product.title
+                                        ? "Fechar"
+                                        : "Saiba Mais"}
+                                </button>
+                                {expandedProduct && expandedProduct.title === product.title && (
+                                    <p className={styles.cardDescription}>{product.description}</p>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- src/app/LoginUser/page.tsx
+++ src/app/LoginUser/page.tsx
@@ -1,18 +1,63 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Link from "next/link";
 import LoginCard from "../components/CardLoginCadastro/page";
 import styles from "./LoginUser.module.css";
 import InputLogin from "../components/input-login/page";
 import ButtonEntrar from "../components/button-entrar/page";
+import { useRouter } from "next/navigation"; 
+
 
 export default function LoginUser() {
+    const [nome, setNome] = useState("");
+    const [senha, setSenha] = useState("");
+    const router = useRouter();
+
+    const handleEntrarClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("https://fakestoreapi.com/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: nome,
+                    password: senha,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log("Token recebido:", data.token);
+
+                router.push("/HomePage");
+            } else {
+                console.error("Erro ao fazer login:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Erro ao fazer login:", error);
+        }
+    };
+
     return (
         <div className={styles.background}>
             <LoginCard title="Entre em sua conta">
                 <form className={styles.form}>
-                    <InputLogin type="email" placeholder="Seu E-mail" />
-                    <InputLogin type="password" placeholder="Sua senha" />
-                    <ButtonEntrar>Entrar</ButtonEntrar>
+                    <InputLogin
+                        type="text" 
+                        placeholder="Seu nome de usuário"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
+                    <InputLogin
+                        type="password"
+                        placeholder="Sua senha"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <ButtonEntrar onClick={handleEntrarClick}>Entrar</ButtonEntrar>
                     <Link href="/CadastroUser" passHref className={styles.redirect}>
                         Ainda não possui conta?
                     </Link>
